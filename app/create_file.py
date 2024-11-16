a6736ccab0a2d28f6b03487853d41c3682bdbe83
Volodymyr Myronets <volodymyrmyronets@gmail.com>
2024-11-16 12:20:53 +0200

    Fixed аÐflags' names and indentation шт ерin the loop

--- app/create_file.py
+++ app/create_file.py
@@ -7,37 +7,37 @@ def create_file(directories: str, file_name: str) -> None:
     os.makedirs(directories, exist_ok=True)
     full_path = os.path.join(directories, file_name)
     mode = "w" if not os.path.exists(full_path) else "a"
-    date_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     line_number = 1
     with open(full_path, mode) as file:
         if mode == "a":
             file.write("\n\n")
-        file.write(f"{date_time}")
+        file.write(f"{timestamp}")
         while True:
             line = input("Enter content line: ")
             if line.lower().strip() == "stop":
                 break
-        file.write(f"{line_number} {line}\n")
-        line_number += 1
+            file.write(f"{line_number} {line}\n")
+            line_number += 1
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
-    parser.add_argument("-d", "directory", type=str, nargs="*")
-    parser.add_argument("-f", "filename", type=str)
+    parser.add_argument("-d", "--directory", type=str, nargs="*")
+    parser.add_argument("-f", "--filename", type=str)
     args = parser.parse_args()
 
-    dirs = args.d
-    file_name = args.f
+    dirs = args.directory
+    file_name = args.filename
 
     if dirs and file_name:
-        directories = os.path.join(*dirs)
+        directories = os.path.join(os.getcwd(), *dirs)
         file_name = file_name
     if not dirs and file_name:
         directories = os.getcwd()
         file_name = file_name
     if not file_name and dirs:
-        directories = os.path.join(*dirs)
+        directories = os.path.join(os.getcwd(), *dirs)
         file_name = "text.txt"
 
     create_file(directories, file_name)
